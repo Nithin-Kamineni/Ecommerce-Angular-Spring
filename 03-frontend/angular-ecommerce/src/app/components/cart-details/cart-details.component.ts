e4a20972c0304ec8c2f8a9acbdfe37e7ae69d7b3
Nithin <nithinkamineni1@gmail.com>
2023-02-07 22:51:15 -0500

    shopping cart details item quantity decrement

--- 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -42,4 +42,11 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.addToCart(theCartItem);
   }
 
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(theCartItem: CartItem) {
+    this.cartService.remove(theCartItem);
+  }
 }
