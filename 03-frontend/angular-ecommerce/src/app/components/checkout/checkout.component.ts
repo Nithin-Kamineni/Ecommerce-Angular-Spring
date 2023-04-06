b48a7eea4935b05344c44f1386e367d5666f1956
Nithin <nithinkamineni1@gmail.com>
2023-04-05 20:34:56 -0400

    stripe payment email support frontend

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -279,7 +279,8 @@ export class CheckoutComponent implements OnInit {
 
     // compute payment info
     this.paymentInfo.amount = Math.round(this.totalPrice * 100);
-    this.paymentInfo.currency = "USD";
+    this.paymentInfo.currency = "USD"; 
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     // if valid form then
     // - create payment intent
